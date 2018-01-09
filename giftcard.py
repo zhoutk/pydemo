163c899abfa0be9e2e767c824e46abc86d6aafed
zhoutk <zhoutk@189.cn>
2018-01-09 10:59:36 +0800

    giftcard adjust.

--- giftcard.py
+++ giftcard.py
@@ -2,7 +2,7 @@ import pymysql
 import random
 
 conn = pymysql.connect(host='172.17.0.1', port=3306, user='root',
-                       passwd='JBiXwMlP9h6@AJ^1', db='onlinetest', charset='utf8mb4')
+                       passwd='123456', db='onlinetest', charset='utf8mb4')
 
 
 def get_n_rand(n):
@@ -24,16 +24,17 @@ def get_rand_pwd(n):
     return rs
 
 
+batnummber = '0105'
 ops = []
-for i in range(50):
+for i in range(131):
     dd = get_n_rand(6)
-    tmp = 'DY1218' + ''.join(dd)
+    tmp = 'DY' + batnummber + ''.join(dd)
     tmp2 = []
     tmp2.append(tmp[0:4])
     tmp2.append(tmp[4:-4])
     tmp2.append(tmp[8:])
 
-    ops.append((i + 3201, 'DY', '1218', tmp, ' '.join(tmp2), ''.join(get_rand_pwd(6))))
+    ops.append((i + 3301, 'DY', batnummber, tmp, ' '.join(tmp2), ''.join(get_rand_pwd(6))))
 
 cur = conn.cursor()
 cur.executemany('insert into `gift_card` (`id`, `flag`, `batch`, `card_number`, `show_number`, `password`) \
